2b8401d12de4f5cc2cdc6b4b78bf9cb03098c5ee
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-05-31 20:35:14 +0530

    adding nav items panel and layout changes with drawer

--- society-management-ui/src/app/core/components/layout/layout.ts
+++ society-management-ui/src/app/core/components/layout/layout.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [MatSidenavModule, MatButtonModule, MatToolbarModule, RouterModule],
+  imports: [MatSidenavModule, MatButtonModule, MatToolbarModule, RouterModule, MatIconModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
   standalone: true,
---
+++ society-management-ui/src/app/core/components/nav-panel/nav-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-panel',
+  imports: [],
+  templateUrl: './nav-panel.html',
+  styleUrl: './nav-panel.scss',
+})
+export class NavPanel {}
