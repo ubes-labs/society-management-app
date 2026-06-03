23743c7b68a5a0136c98543da7b0ee1cb1db05ee
aghosh <aghosh@neogov.net>
2026-06-03 20:12:17 +0530

    feat(nav-panel): enhance navigation links with active state and add titles to routes

--- society-management-ui/src/app/app.routes.ts
+++ society-management-ui/src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { authGuard, navRoutes } from './core';
 
+const appTitle = 'Society Management System';
+
 export const routes: Routes = [
   {
     path: navRoutes.login.route,
+    title: `${navRoutes.login.label} | ${appTitle}`,
     loadComponent: () => import('./features/login/login').then((c) => c.Login),
   },
   {
     path: navRoutes.dashboard.route,
     canActivate: [authGuard],
+    title: `${navRoutes.dashboard.label} | ${appTitle}`,
     loadComponent: () => import('./features/dashboard/dashboard').then((c) => c.Dashboard),
   },
   {
