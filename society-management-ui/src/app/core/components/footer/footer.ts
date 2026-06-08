cad21a2cdc3bd35ebb029c4f837fa5dd28b75b32
aghosh <aghosh@neogov.net>
2026-06-08 21:59:03 +0530

    feat(footer): add app version display and update environment configuration

--- society-management-ui/src/app/core/components/footer/footer.ts
+++ society-management-ui/src/app/core/components/footer/footer.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +9,6 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  readonly appVersion = environment.appVersion;
+}
--- society-management-ui/src/environments/environment.template.ts
+++ society-management-ui/src/environments/environment.template.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  appVersion: '',
   supabaseUrl: '',
   supabaseAnonKey: '',
 };
