950e0fae043afc68b76a7c348aa2fe77f80153ed
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-06-26 22:09:53 +0530

    feat(login): implement signup form with validation and cleanup functionality

--- society-management-ui/src/app/features/login/login.ts
+++ society-management-ui/src/app/features/login/login.ts
@@ -3,7 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { appConst, AuthService } from '../../core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -33,6 +33,13 @@ export class Login {
     password: ['', [Validators.required]],
   });
 
+  readonly signupForm = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    firstname: ['', [Validators.required]],
+    lastname: ['', [Validators.required]],
+  });
+
   async login() {
     await this._auth.loginWithGoogle();
   }
@@ -40,6 +47,19 @@ export class Login {
   async loginWithEmailPassword() {
     if (this.loginForm.valid) {
       console.info(this.loginForm.value);
+      this.loginForm.reset();
+      this.cleanUpForm(this.loginForm);
     }
+  }
+
+  async signupWithEmailPassword() {
+    if (this.signupForm.valid) {
+      console.info(this.signupForm.value);
+      this.cleanUpForm(this.signupForm);
+    }
+  }
+
+  cleanUpForm(form: FormGroup) {
+    form.reset();
   }
 }
