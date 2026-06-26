1d1792a1cc0486844e67a52dc70c8ab8d82f8126
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-06-26 20:52:40 +0530

    feat(login): enhance login form with validation and structure improvements

--- society-management-ui/src/app/features/login/login.ts
+++ society-management-ui/src/app/features/login/login.ts
@@ -2,19 +2,44 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { appConst, AuthService } from '../../core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
   standalone: true,
 })
 export class Login {
   private readonly _auth = inject(AuthService);
+  private readonly _fb = inject(FormBuilder);
+
   readonly appConst = appConst;
+  readonly loginForm = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
   async login() {
     await this._auth.loginWithGoogle();
+  }
+
+  async loginWithEmailPassword() {
+    if (this.loginForm.valid) {
+      console.info(this.loginForm.value);
+    }
   }
 }
